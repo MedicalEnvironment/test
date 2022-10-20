ff162235178358cfe29353a0f7f6b7ffc25ea387
MedicalEnvironment <105016091+MedicalEnvironment@users.noreply.github.com>
2022-10-20 15:34:10 +0300

    Testing atm

---
+++ main.cpp
@@ -0,0 +1,64 @@
+//
+//  main.cpp
+//  IntermediateLevel
+//
+//  Created by Akbar Abayev on 26.09.2022.
+//
+
+#include <iostream>
+using namespace std;
+// std::cout can be terminated in this format 
+
+class Shape {
+public:
+  Shape() {}
+  //defining a virtual function called Draw for shape class
+  virtual void Draw() { std::cout << "Drawing a Shape" << std::endl; }
+};
+
+class Rectangle : public Shape {
+public:
+  Rectangle() {}
+  //Draw function defined for Rectangle class
+  virtual void Draw() { std::cout << "Drawing a Rectangle" << std::endl; }
+};
+
+class Triangle : public Shape {
+public:
+  Triangle() {}
+  //Draw function defined for Triangle class
+  virtual void Draw() { std::cout << "Drawing a Triangle" << std::endl; }
+};
+
+class Circle : public Shape {
+public:
+  Circle() {}
+  //Draw function defined for Circle class
+  virtual void Draw() { std::cout << "Drawing a Circle" << std::endl; }
+};
+
+int main()
+{
+  Shape* s;
+
+  Triangle  tri;
+  Rectangle rec;
+  Circle    circ;
+
+  // store the address of Rectangle
+  s = &rec;
+  // call Rectangle Draw function
+  s->Draw();
+
+  // store the address of Triangle
+  s = &tri;
+  // call Traingle Draw function
+  s->Draw();
+
+  // store the address of Circle
+  s = &circ;
+  // call Circle Draw function
+  s->Draw();
+
+  return 0;
+}
